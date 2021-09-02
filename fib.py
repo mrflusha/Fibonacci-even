c967742abe7b9fbdce01fd5a505b4123523e75a1
Sergey <46491304+mrflusha@users.noreply.github.com>
2021-09-02 17:32:43 +0300

    function Fibonacci even

--- fib.py
+++ fib.py
@@ -1,28 +1,15 @@
 def fib(x):
-	a = 0
-	b = 1
-	for i in range(x):
-		a,b = b, a+b 
-		print(a)
-
-	return a
+	fib_1 = 0
+	fib_2 = 1
+	fib_even = [0]
 
 
+	while len(fib_even) < x:
+		if fib_2 % 2 == 0:
+			fib_even.append(fib_2)
+		fib_sum = fib_1 + fib_2 
+		fib_1 = fib_2
+		fib_2 = fib_sum
+		#print(fib_even)
 
-#out = fib(4) 
-
-#print (out)
-
-fib_1 = 0
-fib_2 = 1
-fib = list()
-i = 0
-x = 20
-while i < x :
-	if fib_2 % 2 == 0:
-		fib.append(fib_2)
-	fib_sum = fib_1 + fib_2 
-	fib_1 = fib_2
-	fib_2 = fib_sum
-	i +=1
-	print(fib)
+	return fib_even
